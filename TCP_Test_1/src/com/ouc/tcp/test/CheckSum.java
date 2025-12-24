8dae30ec7289865e1a3c7b9e4dfd52f5d7105f42
chen <718831560@qq.com>
2025-12-24 11:50:30 +0800

    RDT2.0完整修改运行后

--- TCP_Test_1/src/com/ouc/tcp/test/CheckSum.java
+++ TCP_Test_1/src/com/ouc/tcp/test/CheckSum.java
@@ -6,14 +6,27 @@ import com.ouc.tcp.message.TCP_HEADER;
 import com.ouc.tcp.message.TCP_PACKET;
 
 public class CheckSum {
-	
-	/*计算TCP报文段校验和：只需校验TCP首部中的seq、ack和sum，以及TCP数据字段*/
-	public static short computeChkSum(TCP_PACKET tcpPack) {
-		int checkSum = 0;
-		
-		
-		
-		return (short) checkSum;
-	}
+
+    /*计算TCP报文段校验和：只需校验TCP首部中的seq、ack和sum，以及TCP数据字段*/
+    public static short computeChkSum(TCP_PACKET tcpPack) {
+        // 使用 CRC32 算法进行校验
+        CRC32 crc = new CRC32();
+
+        // 1. 校验首部关键字段
+        crc.update(tcpPack.getTcpH().getTh_seq());
+        crc.update(tcpPack.getTcpH().getTh_ack());
+
+        // 2. 校验数据部分
+        // 注意：Data是int[]，需要遍历加入校验
+        int[] data = tcpPack.getTcpS().getData();
+        if (data != null) {
+            for (int i = 0; i < data.length; i++) {
+                crc.update(data[i]);
+            }
+        }
+
+        // 3. 返回校验值 (强制转换为short)
+        return (short) crc.getValue();
+    }
 	
 }
--- TCP_Test_1/src/com/ouc/tcp/test/TCP_Sender.java
+++ TCP_Test_1/src/com/ouc/tcp/test/TCP_Sender.java
@@ -34,10 +34,10 @@ public class TCP_Sender extends TCP_Sender_ADT {
 		
 		//发送TCP数据报
 		udt_send(tcpPack);
-		flag = 0;
+		flag = 0;// 重置标志位，准备进入等待
 		
 		//等待ACK报文
-		//waitACK();
+		waitACK();
 		while (flag==0);
 	}
 	
@@ -45,7 +45,7 @@ public class TCP_Sender extends TCP_Sender_ADT {
 	//不可靠发送：将打包好的TCP数据报通过不可靠传输信道发送；仅需修改错误标志
 	public void udt_send(TCP_PACKET stcpPack) {
 		//设置错误控制标志
-		tcpH.setTh_eflag((byte)0);		
+		tcpH.setTh_eflag((byte)1);
 		//System.out.println("to send: "+stcpPack.getTcpH().getTh_seq());				
 		//发送数据报
 		client.send(stcpPack);
