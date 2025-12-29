7e6d15deff28bd68a235f8d7dd812f289f99a9bc
chen <718831560@qq.com>
2025-12-30 00:58:58 +0800

    3.0

--- TCP_Test_1/src/com/ouc/tcp/test/TCP_Sender.java
+++ TCP_Test_1/src/com/ouc/tcp/test/TCP_Sender.java
@@ -1,3 +1,6 @@
+/***************************2.1: ACK/NACK
+**************************** Feng Hong; 2015-12-09*/
+
 package com.ouc.tcp.test;
 
 import com.ouc.tcp.client.TCP_Sender_ADT;
@@ -8,86 +11,88 @@ import com.ouc.tcp.tool.TCP_TOOL;
 
 public class TCP_Sender extends TCP_Sender_ADT {
 
-    private TCP_PACKET tcpPack;	//待发送的TCP数据报
-    private volatile int flag = 1;
+	private TCP_PACKET tcpPack;	//待发送的TCP数据报
+	private volatile int flag = 0;
 
-    private SenderSlidingWindow window = new SenderSlidingWindow(this.client);
+	/*构造函数*/
+	public TCP_Sender() {
+		super();	//调用超类构造函数
+		super.initTCP_Sender(this);		//初始化TCP发送端
+	}
 
+	@Override
+	//可靠发送（应用层调用）：封装应用层数据，产生TCP数据报；需要修改
+	public void rdt_send(int dataIndex, int[] appData) {
 
-    /*构造函数*/
-    public TCP_Sender() {
-        super();	//调用超类构造函数
-        super.initTCP_Sender(this);		//初始化TCP发送端
-    }
+		//生成TCP数据报（设置序号和数据字段/校验和),注意打包的顺序
+		tcpH.setTh_seq(dataIndex * appData.length + 1);//包序号设置为字节流号：
+		tcpS.setData(appData);
+		tcpPack = new TCP_PACKET(tcpH, tcpS, destinAddr);
+		//更新带有checksum的TCP 报文头
+		tcpH.setTh_sum(CheckSum.computeChkSum(tcpPack));
+		tcpPack.setTcpH(tcpH);
 
-    @Override
-    //可靠发送（应用层调用）：封装应用层数据，产生TCP数据报；需要修改
-    public void rdt_send(int dataIndex, int[] appData) {
+		//发送TCP数据报
+		udt_send(tcpPack);
+		flag = 0;// 重置标志位，准备进入等待
 
-        //生成TCP数据报（设置序号和数据字段/校验和),注意打包的顺序
-        tcpH.setTh_seq(dataIndex * appData.length + 1);//包序号设置为字节流号：
-        tcpS.setData(appData);
-        tcpPack = new TCP_PACKET(tcpH, tcpS, destinAddr);
-        //更新带有checksum的TCP 报文头
-        tcpH.setTh_sum(CheckSum.computeChkSum(tcpPack));
-        tcpPack.setTcpH(tcpH);
+		//等待ACK报文
+		//waitACK();
+		while (flag==0);
+	}
 
-        // 判断发送窗口是否已满
-        if (this.window.isFull()) {
-            System.out.println();
-            System.out.println("Sliding Window is full");
-            System.out.println();
-            this.flag = 0;
+	@Override
+	//不可靠发送：将打包好的TCP数据报通过不可靠传输信道发送；仅需修改错误标志
+	public void udt_send(TCP_PACKET stcpPack) {
+		//设置错误控制标志
+		tcpH.setTh_eflag((byte)1);
+		//System.out.println("to send: "+stcpPack.getTcpH().getTh_seq());
+		//发送数据报
+		client.send(stcpPack);
+	}
+
+	@Override
+	//需要修改
+	public void waitACK() {
+		//循环检查ackQueue
+		//循环检查确认号对列中是否有新收到的ACK
+		if(!ackQueue.isEmpty()){
+			int currentAck=ackQueue.poll();
+			// System.out.println("CurrentAck: "+currentAck);
+			if (currentAck == tcpPack.getTcpH().getTh_seq()){
+				System.out.println("Clear: "+tcpPack.getTcpH().getTh_seq());
+				flag = 1;// 收到正确ACK，停止等待
+				//break;
+			}else{
+                // 若收到冗余 ACK (如发101收到ACK 1) 则说明接收方没收到101
+                // 若收到损坏 ACK (被 recv 标记为 -2)则必须重传
+                System.out.println("RDT 2.2 Sender: Duplicate/Corrupt ACK received. Retransmit: " + tcpPack.getTcpH().getTh_seq());
+				udt_send(tcpPack);
+				flag = 0;
+			}
+		}
+	}
+
+	@Override
+	//接收到ACK报文：检查校验和，将确认号插入ack队列;NACK的确认号为－1；不需要修改
+	public void recv(TCP_PACKET recvPack) {
+        // 1. 检查 ACK 包是否损坏（校验和）
+        if (CheckSum.computeChkSum(recvPack) == recvPack.getTcpH().getTh_sum()) {
+            // 完好，提取确认号
+            System.out.println("Receive ACK Number: " + recvPack.getTcpH().getTh_ack());
+            ackQueue.add(recvPack.getTcpH().getTh_ack());
+        } else {
+            // 损坏：ACK 坏了，发送方无法确认接收方状态
+            // 在 RDT 2.2 中，ACK 坏了等同于没收到正确 ACK，触发重传
+            // 这里的 -2 是一个自定义的“错误标记”
+            System.out.println("RDT 2.2 Sender: Corrupted ACK. Triggering retransmission.");
+            ackQueue.add(-2);
         }
 
-        //等待ACK报文
-        while (flag==0);
+        System.out.println();
+	    //处理ACK报文
+	    waitACK();
 
-        try {
-            this.window.putPacket(this.tcpPack.clone());
-        } catch (CloneNotSupportedException e) {
-            e.printStackTrace();
-        }
-
-        // 发送 TCP 数据报
-        udt_send(this.tcpPack);
-
-    }
-
-    @Override
-    //不可靠发送：将打包好的TCP数据报通过不可靠传输信道发送；仅需修改错误标志
-    public void udt_send(TCP_PACKET stcpPack) {
-        //设置错误控制标志
-        // 设置错误控制标志
-        // 0: 信道无差错
-        // 1: 只出错
-        // 2: 只丢包
-        // 3: 只延迟
-        // 4: 出错 / 丢包
-        // 5: 出错 / 延迟
-        // 6: 丢包 / 延迟
-        // 7: 出错 / 丢包 / 延迟
-        tcpH.setTh_eflag((byte)7);
-        //System.out.println("to send: "+stcpPack.getTcpH().getTh_seq());
-        //发送数据报
-        client.send(stcpPack);
-    }
-
-    @Override
-    //需要修改
-    public void waitACK() {	}
-
-    @Override
-    //接收到ACK报文：
-    public void recv(TCP_PACKET recvPack) {
-        if (CheckSum.computeChkSum(recvPack) == recvPack.getTcpH().getTh_sum()) {  // 检查校验和
-            System.out.println("Receive ACK Number： "+ recvPack.getTcpH().getTh_ack());
-
-            window.receiveACK((recvPack.getTcpH().getTh_ack() - 1) / 100);
-            if (!window.isFull()) {
-                this.flag = 1;
-            }
-        }
-    }
+	}
 
 }
