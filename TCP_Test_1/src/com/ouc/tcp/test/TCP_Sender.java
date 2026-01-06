4e5776e7b4df3504c5da836ba70a0964bf40fcb4
chen <718831560@qq.com>
2026-01-06 23:47:25 +0800

    2.2

--- TCP_Test_1/src/com/ouc/tcp/test/TCP_Sender.java
+++ TCP_Test_1/src/com/ouc/tcp/test/TCP_Sender.java
@@ -37,8 +37,8 @@ public class TCP_Sender extends TCP_Sender_ADT {
 		flag = 0;// 重置标志位，准备进入等待
 		
 		//等待ACK报文
-		//waitACK();
-		while (flag==0);
+		waitACK();
+		//while (flag==0);
 	}
 	
 	@Override
